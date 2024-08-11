40df7bec70ee066ee2ab39b5cd91bb7ff342a028
amr1abo2hassab3 <amrabohassab123@gmail.com>
2024-08-12 02:50:31 +0300

    Edit 1

--- vendor/js/main.js
+++ vendor/js/main.js
@@ -20,6 +20,8 @@ function AddActive(e) {
 
   // إضافة الصنف "active" للعنصر الذي تم النقر عليه
   e.target.classList.add("active");
+  menuIcon.classList.toggle("bx-x");
+  navBar.classList.toggle("navbarActive");
 }
 
 // ربط الدالة بحدث النقر على كل رابط
@@ -52,3 +54,74 @@ const typed = new Typed(".multiple-text", {
   backDelay: 1000,
   loop: true,
 });
+
+///////////////////////////////////////////////////
+
+btnColor = document.querySelector(".color-switcher-icon");
+boxColor = document.querySelector(".change-color");
+iconColor = document.querySelectorAll(".color-switcher-icon i");
+
+// show button
+btnColor?.addEventListener("click", () => {
+  boxColor?.classList.toggle("showBtn");
+  if (iconColor.length > 1) {
+    iconColor[1].classList.toggle("bx-chevron-right");
+  }
+});
+
+// change color in page
+
+let colorPage = document.querySelectorAll(".change-color div");
+
+// this function change color to  < blue  -  default >
+colorPage[0].addEventListener("click", () => {
+  document.documentElement.style.setProperty("--main-color", "#0ef");
+  document.documentElement.style.setProperty("--text-color", "#fff");
+  document.documentElement.style.setProperty("--second-bg-color", "#323946");
+  document.documentElement.style.setProperty("--bg-color", "#1f242d");
+});
+// this function change color to  < green >
+colorPage[1].addEventListener("click", () => {
+  document.documentElement.style.setProperty("--main-color", "#00ff51");
+  document.documentElement.style.setProperty("--text-color", "#fff");
+  document.documentElement.style.setProperty("--second-bg-color", "#001005");
+  document.documentElement.style.setProperty("--bg-color", "#080808");
+});
+// this function change color to  < red >
+colorPage[2].addEventListener("click", () => {
+  document.documentElement.style.setProperty("--main-color", "#f60b0b");
+  document.documentElement.style.setProperty("--text-color", "#fff");
+  document.documentElement.style.setProperty("--second-bg-color", "#1b0000");
+  document.documentElement.style.setProperty("--bg-color", "#080808");
+});
+// this function change color to  < yellow >
+colorPage[3].addEventListener("click", () => {
+  document.documentElement.style.setProperty("--main-color", "#ea580c");
+  document.documentElement.style.setProperty("--text-color", "#fff");
+  document.documentElement.style.setProperty("--second-bg-color", "#101010");
+  document.documentElement.style.setProperty("--bg-color", "#080808");
+});
+
+// go to the top page
+
+let btnTop = document.getElementById("btn-top");
+
+window.onscroll = function () {
+  if (scrollY >= 993) {
+    btnTop.style.display = "block";
+  } else {
+    btnTop.style.display = "none";
+  }
+};
+
+btnTop.onclick = () => {
+  scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+  for (let i = 0; i < clickActive.length; i++) {
+    clickActive[i].classList.remove("active");
+  }
+  clickActive[0].classList.add("active");
+};
